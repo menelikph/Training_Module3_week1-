e7f146c8c827e75b0b86dcbcca83da57115eda7a
menelik <menelikdev@gmail.com>
2025-06-18 18:49:23 -0500

    Refactor HTML structure and enhance form validation in JavaScript

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-console.log("Bienvenido al Sistema Interactivo de Mensajes!");
+// console.log("Bienvenido al Sistema Interactivo de Mensajes!");
+// let nameUser = prompt("cuales tu nombre?: ");
+// let age = prompt("digite su edad: ");
 
-let nameUser = prompt("cuales tu nombre?: ");
-let age = prompt("digite su edad: ");
-
-age = parseInt(age);
-
-function is_negative_number(age){
-  if(age < 0){
-    return true
-  }else{
-    return false
+function is_negative_number(age) {
+  if (age < 0) {
+    return true;
+  } else {
+    return false;
   }
 }
 
-if ((isNaN(age) || is_negative_number(age)) ) {
-  console.error("Error: Por favor, ingresa una edad valida.");
-} else if (age < 18) {
-  alert(
-    `Hola ${nameUser}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del codigo`
-  );
-} else {
-  alert(
-    `Hola ${nameUser}, eres mayor de edad. ¡preparate para grandes oportunidades en el mundo de la prograacion!`
-  );
+function validateForm(form, event) {
+  event.preventDefault();
+
+  let nameUser = form.UserName.value;
+  let age = +form.age.value;
+
+  console.log(nameUser, age);
+
+  const isNegative = is_negative_number(age); // Verifica si la edad es negativa
+
+  if (isNegative || isNaN(age) || is_negative_number(age)) {
+    // Verifica si la edad es negativa o no es un número
+    console.error("Error: Por favor, ingresa una edad valida.");
+  } else if (age < 18) {
+    document.getElementById(
+      "result"
+    ).innerHTML = `Hola ${nameUser}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del codigo`;
+
+    document.getElementById(
+      "result"
+    ).innerHTML = `Hola ${nameUser}, eres mayor de edad. ¡preparate para grandes oportunidades en el mundo de la programacion!`;
+  }
 }
